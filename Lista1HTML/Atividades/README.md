13b93b30f85a3eb0901394f11a7450a0c3e8c973
Pedrogitbuy <140627568+Pedrogitbuy@users.noreply.github.com>
2023-08-28 09:05:12 -0300

    Delete Lista1HTML directory

--- Lista1HTML/Atividades/README.md
+++
@@ -1,8 +0,0 @@
-# Atividades
----
-##  Repositório com códigos e exercícios C, HTML
-- Para executar utilize DevC++ e/ou um Browser
-
-|C|HTML|
-|-|-|
-|DevC++|Browser|
--- Lista1HTML/Atividades/old/Visual.html/teste/apps/apps.js
+++
@@ -1,5 +0,0 @@
-function mostrarData() {
-    const dataAtual = new Date();
-    const elementoData = document.getElementById("data-atual");
-    elementoData.textContent = "Data atual: " + dataAtual.toLocaleDateString();
-}
